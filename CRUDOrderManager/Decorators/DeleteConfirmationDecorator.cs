a56b98d680368e6f80420f586a64ec48ac0ebafe
Yoelvis Mulen <ymulenll@hotmail.com>
2017-06-27 12:44:07 -0300

    added example of NotImplementedException

---
+++ CRUDOrderManager/Decorators/DeleteConfirmationDecorator.cs
@@ -0,0 +1,51 @@
+﻿using Domain.Interfaces;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorators
+{
+    public class DeleteConfirmationDecorator<TEntity> : ICreateReadUpdateDelete<TEntity>
+    {
+        private readonly ICreateReadUpdateDelete<TEntity> decoratedCrud;
+
+        private readonly IUserInteraction userInteraction;
+
+        public DeleteConfirmationDecorator(ICreateReadUpdateDelete<TEntity> decoratedCrud, IUserInteraction userInteraction)
+        {
+            this.decoratedCrud = decoratedCrud;
+            this.userInteraction = userInteraction;
+        }
+
+        public void Create(TEntity entity)
+        {
+            this.decoratedCrud.Create(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            if (this.userInteraction.Confirm("Are you sure you want to delete de entry ?"))
+            {
+                decoratedCrud.Delete(entity);
+            }
+        }
+
+        public IEnumerable<TEntity> ReadAll()
+        {
+            return this.decoratedCrud.ReadAll();
+        }
+
+        public TEntity ReadOne(int id)
+        {
+            return this.decoratedCrud.ReadOne(id);
+        }
+
+        public void Update(TEntity entity)
+        {
+            this.decoratedCrud.Update(entity);
+        }
+    }
+}
---
+++ CRUDOrderManager/Repository.Implementation/NewDeleteOrder.cs
@@ -0,0 +1,37 @@
+﻿using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Implementation
+{
+    public class NewDeleteOrder<TEntity> : ICreateReadUpdateDelete<TEntity>
+    {
+        public void Create(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            // New Implementation
+        }
+
+        public IEnumerable<TEntity> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity ReadOne(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
