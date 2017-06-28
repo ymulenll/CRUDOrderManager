fad5c8a3cf167cf5ea43fd696c6dff15c19bcd53
Yoelvis Mulen <ymulenll@hotmail.com>
2017-06-28 11:22:06 -0300

    add tests not implemented exception.

---
+++ CRUDOrderManager/Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4722916e-04f5-4bb7-b19b-315278ebd7d9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CRUDOrderManager/Tests/UnitTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Repository.Implementation;
+using Model;
+using Repository.Interfaces;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestNotImplementedException()
+        {
+            ICreateReadUpdateDelete<Order> crud = new EmptyCreateReadUpdateDelete<Order>();
+            //ICreateReadUpdateDelete<Order> crud = new NewDeleteOrder<Order>();
+
+            crud.ReadAll();
+        }
+
+        [TestMethod]
+        public void TestNotImplementedExceptionLists()
+        {
+            ICollection<Order> orders = GetOrders();
+
+            var expectedCount = orders.Count + 1;
+
+            orders.Add(new Order());
+
+            Assert.AreEqual(expectedCount, orders.Count);
+        }
+
+        private ICollection<Order> GetOrders()
+        {
+            //return new List<Order>(new[] { new Order() });
+            return new ReadOnlyCollection<Order>(new[] { new Order() });
+        }
+    }
+}
